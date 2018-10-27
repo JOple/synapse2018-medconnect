783ca4117a702bf2a1e15f04d5be93da3eb1dc5e
JOple <jose_jaena_ople@dlsu.edu.ph>
2018-10-27 18:36:04 +0800

    Added some folders

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as express from "express"
 import uuidv4 = require('uuid/v4')
 
 import { config } from "./config";
-import { personalAccounts } from "./db/accounts";
+import { personalAccounts, medicalAccounts } from "./db/accounts";
 
 export const server = express()
 
@@ -56,9 +56,59 @@ server.post("/personal_login", (req, res) => {
 
     let accessKey = uuidv4();
     cache[accessKey] = new Set(account.resources)
+
+    res.json({
+        accessKey: accessKey
+    })
 })
 server.post("/medical_login", (req, res) => {
+    let body;
+    try {
+        body = typeof req.body == "string" ? JSON.parse(req.body) : req.body
+    } catch (e) {
+        res.status(400).json({
+            "error": "Bad Request"
+        })
+    }
 
+    let username = body.username
+    let password = body.password
+    let resourceId = body.resourceId
+
+    if (!username || !password) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+    if (!resourceId) {
+        res.status(403).json({
+            "error": "Invalid requested resource"
+        })
+        return
+    }
+
+    let account = medicalAccounts[username]
+
+    if (!account) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+    if (password != account.password) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+
+    let accessKey = uuidv4();
+    cache[accessKey] = new Set(account.resources)
+
+    res.json({
+        accessKey: accessKey
+    })
 })
 
 server.get("/records/:id", (req, res) => {
