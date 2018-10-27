8d6ee001a30eb04158b2181656a260d42d7eb936
JOple <jose_jaena_ople@dlsu.edu.ph>
2018-10-27 18:49:27 +0800

    Edited folder structure

--- config.ts
+++ config.ts
@@ -1,4 +1,9 @@
 export const config = {
 
-    allowRecordAccessHeader: "X-Allow-Record-Access"
+    allowRecordAccessHeader: "X-Allow-Record-Access",
+    debug: true,
+    sessionSecret: "saytd85vb0y4nt89yerm9up9ruvgm-450mpuc9rglmfdupirepjw5f30mwerirlmlfkhwklmredwrwdehkl",
+    rootDir: __dirname
 }
+
+export default config
---
+++ modules/module-accesspoints.ts
@@ -0,0 +1,137 @@
+import app from "../index"
+import config from "../config";
+
+import uuidv4 = require('uuid/v4')
+
+import { personalAccounts, medicalAccounts } from "../db/accounts";
+
+interface AccessRights {
+    [key: string]: Set<string>
+}
+
+const cache: AccessRights = {}
+
+app.post("/personal_login", (req, res) => {
+    let body;
+    try {
+        body = typeof req.body == "string" ? JSON.parse(req.body) : req.body
+    } catch (e) {
+        res.status(400).json({
+            "error": "Bad Request"
+        })
+    }
+
+    let username = body.username
+    let password = body.password
+    let resourceId = body.resourceId
+
+    if (!username || !password) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+    if (!resourceId) {
+        res.status(403).json({
+            "error": "Invalid requested resource"
+        })
+        return
+    }
+
+    let account = personalAccounts[username]
+
+    if (!account) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+    if (password != account.password) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+
+    let accessKey = uuidv4();
+    cache[accessKey] = new Set(account.resources)
+
+    res.json({
+        accessKey: accessKey
+    })
+})
+app.post("/medical_login", (req, res) => {
+    let body;
+    try {
+        body = typeof req.body == "string" ? JSON.parse(req.body) : req.body
+    } catch (e) {
+        res.status(400).json({
+            "error": "Bad Request"
+        })
+    }
+
+    let username = body.username
+    let password = body.password
+    let resourceId = body.resourceId
+
+    if (!username || !password) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+    if (!resourceId) {
+        res.status(403).json({
+            "error": "Invalid requested resource"
+        })
+        return
+    }
+
+    let account = medicalAccounts[username]
+
+    if (!account) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+    if (password != account.password) {
+        res.status(403).json({
+            "error": "Invalid username or password"
+        })
+        return
+    }
+
+    let accessKey = uuidv4();
+    cache[accessKey] = new Set([...account.resources, resourceId])
+
+    res.json({
+        accessKey: accessKey
+    })
+})
+
+app.get("/records/:id", (req, res) => {
+    let id = req.param("id")
+
+    if (!id) {
+        res.redirect("/invalid_url")
+        return
+    }
+
+    let accessKey = req.header(config.allowRecordAccessHeader)
+    let allowedRecords = cache[accessKey]
+
+    if (!allowedRecords || allowedRecords.size == 0) {
+        res.redirect("/login/" + id)
+        return
+    }
+})
+app.get("/login/:id", (req, res) => {
+
+})
+app.get("/invalid_url", (req, res) => {
+
+})
+app.get("*", (req, res) => {
+    res.redirect("/invalid_url")
+})
