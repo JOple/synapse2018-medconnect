2159485dadf8d054ef0246f11e393f853fbe501a
JOple <jose_jaena_ople@dlsu.edu.ph>
2018-12-12 16:22:44 +0800

    Some clean up

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ export const config = {
     debug: true,
     sessionSecret: "saytd85vb0y4nt89yerm9up9ruvgm-450mpuc9rglmfdupirepjw5f30mwerirlmlfkhwklmredwrwdehkl",
     rootDir: __dirname,
-    port: 9200
+    port: process.env.PORT || 9200
 }
 
 export default config
